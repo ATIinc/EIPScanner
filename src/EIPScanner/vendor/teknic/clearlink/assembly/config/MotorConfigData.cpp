a11bf8458be1ee473dc2257ae5e937acb166cbb3
jan-ati <jritzenhoff@ati-inc.co>
2024-12-05 20:03:29 +0000

    Added MotorConfig getters/setters

--- src/EIPScanner/vendor/teknic/clearlink/assembly/config/MotorConfigData.cpp
+++ src/EIPScanner/vendor/teknic/clearlink/assembly/config/MotorConfigData.cpp
@@ -36,6 +36,146 @@ MotorConfigData::_getDataFieldReferences() {
           std::ref(_followAxis),
           std::ref(_reservedPadding)};
 }
+
+// --------------------------------------------
+
+eipScanner::cip::CipDword MotorConfigData::getConfigRegister() const {
+  return _configRegister;
+}
+
+void MotorConfigData::setConfigRegisterFlag(ConfigFlag flag, bool value) {
+  if (value) {
+    _configRegister |= (1 << flag);
+  } else {
+    _configRegister &= ~(1 << flag);
+  }
+}
+
+void MotorConfigData::setConfigRegister(
+    eipScanner::cip::CipDword configRegister) {
+  _configRegister = configRegister;
+}
+
+// --------------------------------------------
+
+eipScanner::cip::CipDint MotorConfigData::getFollowDivisor() const {
+  return _followDivisor;
+}
+void MotorConfigData::setFollowDivisor(eipScanner::cip::CipDint followDivisor) {
+  _followDivisor = followDivisor;
+}
+
+// --------------------------------------------
+
+eipScanner::cip::CipDint MotorConfigData::getFollowMultiplier() const {
+  return _followMultiplier;
+}
+void MotorConfigData::setFollowMultiplier(
+    eipScanner::cip::CipDint followMultiplier) {
+  _followMultiplier = followMultiplier;
+}
+
+// --------------------------------------------
+
+eipScanner::cip::CipDint MotorConfigData::getMaxDeceleration() const {
+  return _maxDeceleration;
+}
+void MotorConfigData::setMaxDeceleration(
+    eipScanner::cip::CipDint maxDecelerationRate) {
+  _maxDeceleration = maxDecelerationRate;
+}
+
+// --------------------------------------------
+
+eipScanner::cip::CipDint MotorConfigData::getSoftLimitPosition1() const {
+  return _softLimitPosition1;
+}
+void MotorConfigData::setSoftLimitPosition1(
+    eipScanner::cip::CipDint softLimitPosition1) {
+  _softLimitPosition1 = softLimitPosition1;
+}
+
+// --------------------------------------------
+
+eipScanner::cip::CipDint MotorConfigData::getSoftLimitPosition2() const {
+  return _softLimitPosition2;
+}
+void MotorConfigData::setSoftLimitPosition2(
+    eipScanner::cip::CipDint softLimitPosition2) {
+  _softLimitPosition2 = softLimitPosition2;
+}
+
+// --------------------------------------------
+
+eipScanner::cip::CipSint MotorConfigData::getPositiveLimitConnector() const {
+  return _positiveLimitConnector;
+}
+void MotorConfigData::setPositiveLimitConnector(
+    eipScanner::cip::CipSint positiveLimitConnector) {
+  _positiveLimitConnector = positiveLimitConnector;
+}
+
+// --------------------------------------------
+
+eipScanner::cip::CipSint MotorConfigData::getNegativeLimitConnector() const {
+  return _negativeLimitConnector;
+}
+void MotorConfigData::setNegativeLimitConnector(
+    eipScanner::cip::CipSint negativeLimitConnector) {
+  _negativeLimitConnector = negativeLimitConnector;
+}
+
+// --------------------------------------------
+
+eipScanner::cip::CipSint MotorConfigData::getHomeSensorConnector() const {
+  return _homeSensorConnector;
+}
+void MotorConfigData::setHomeSensorConnector(
+    eipScanner::cip::CipSint homeSensorConnector) {
+  _homeSensorConnector = homeSensorConnector;
+}
+
+// --------------------------------------------
+
+eipScanner::cip::CipSint MotorConfigData::getBrakeOutputConnector() const {
+  return _brakeOutputConnector;
+}
+void MotorConfigData::setBrakeOutputConnector(
+    eipScanner::cip::CipSint brakeOutputConnector) {
+  _brakeOutputConnector = brakeOutputConnector;
+}
+
+// --------------------------------------------
+
+eipScanner::cip::CipSint MotorConfigData::getStopSensorConnector() const {
+  return _stopSensorConnector;
+}
+void MotorConfigData::setStopSensorConnector(
+    eipScanner::cip::CipSint stopSensorConnector) {
+  _stopSensorConnector = stopSensorConnector;
+}
+
+// --------------------------------------------
+
+eipScanner::cip::CipSint
+MotorConfigData::getTriggerPositionCaptureConnector() const {
+  return _triggerPositionCaptureConnector;
+}
+void MotorConfigData::setTriggerPositionCaptureConnector(
+    eipScanner::cip::CipSint triggerPositionCaptureConnector) {
+  _triggerPositionCaptureConnector = triggerPositionCaptureConnector;
+}
+
+// --------------------------------------------
+
+eipScanner::cip::CipSint MotorConfigData::getFollowAxis() const {
+  return _followAxis;
+}
+void MotorConfigData::setFollowAxis(
+    eipScanner::cip::CipSint followEncoderMode) {
+  _followAxis = followEncoderMode;
+}
+
 } // namespace config
 } // namespace assembly
 } // namespace clearlink
--- src/EIPScanner/vendor/teknic/clearlink/assembly/config/MotorConfigData.h
+++ src/EIPScanner/vendor/teknic/clearlink/assembly/config/MotorConfigData.h
@@ -33,6 +33,182 @@ public:
    */
   MotorConfigData();
 
+  /**
+   * @brief Gets Config Register [AttrID=1]
+   * @return
+   */
+  eipScanner::cip::CipDword getConfigRegister() const;
+
+  /**
+   * @brief Gets SoftLimitPosition1 [AttrID=2]
+   * @return
+   */
+  eipScanner::cip::CipDint getSoftLimitPosition1() const;
+
+  /**
+   * @brief Gets SoftLimitPosition2 [AttrID=3]
+   * @return
+   */
+  eipScanner::cip::CipDint getSoftLimitPosition2() const;
+
+  /**
+   * @brief Gets PositiveLimitConnector [AttrID=4]
+   * @return
+   */
+  eipScanner::cip::CipSint getPositiveLimitConnector() const;
+
+  /**
+   * @brief Gets NegativeLimitConnector [AttrID=5]
+   * @return
+   */
+  eipScanner::cip::CipSint getNegativeLimitConnector() const;
+
+  /**
+   * @brief Gets HomeSensorConnector [AttrID=6]
+   * @return
+   */
+  eipScanner::cip::CipSint getHomeSensorConnector() const;
+
+  /**
+   * @brief Gets BrakeOutputConnector [AttrID=7]
+   * @return
+   */
+  eipScanner::cip::CipSint getBrakeOutputConnector() const;
+
+  /**
+   * @brief Gets TriggerPositionCaptureConnector [AttrID=8]
+   * @return
+   */
+  eipScanner::cip::CipSint getTriggerPositionCaptureConnector() const;
+
+  /**
+   * @brief Gets MaxDecelerationRate [AttrID=9]
+   * @return
+   */
+  eipScanner::cip::CipDint getMaxDeceleration() const;
+
+  /**
+   * @brief Gets StopSensorConnector [AttrID=10]
+   * @return
+   */
+  eipScanner::cip::CipSint getStopSensorConnector() const;
+
+  /**
+   * @brief Gets FollowEncoderMode [AttrID=11]
+   * @return
+   */
+  eipScanner::cip::CipSint getFollowAxis() const;
+
+  /**
+   * @brief Gets FollowDivisor [AttrID=12]
+   * @return
+   */
+  eipScanner::cip::CipDint getFollowDivisor() const;
+
+  /**
+   * @brief Gets FollowMultiplier [AttrID=13]
+   * @return
+   */
+  eipScanner::cip::CipDint getFollowMultiplier() const;
+
+  // -------------------------------------------------------------
+
+  enum ConfigFlag {
+    HomingEnable = 0,
+    HomeSensorActiveLevel = 1,
+    EnableInversion = 2,
+    HLFBInversion = 3, // NOTE: The default if HIGH
+    PositionCaptureActiveLevel = 4,
+    SoftwareLimitEnable = 5
+    // 6 - 31 are reserved
+  };
+
+  /**
+   * @brief Sets a motor config flag
+   */
+  void setConfigRegisterFlag(ConfigFlag flag, bool value);
+
+  /**
+   * @brief Sets Config Register [AttrID=1]
+   * @return
+   */
+  void setConfigRegister(eipScanner::cip::CipDword configRegister);
+
+  /**
+   * @brief Sets SoftLimitPosition1 [AttrID=2]
+   * @return
+   */
+  void setSoftLimitPosition1(eipScanner::cip::CipDint softLimitPosition1);
+
+  /**
+   * @brief Sets SoftLimitPosition2 [AttrID=3]
+   * @return
+   */
+  void setSoftLimitPosition2(eipScanner::cip::CipDint softLimitPosition2);
+
+  /**
+   * @brief Sets PositiveLimitConnector [AttrID=4]
+   * @return
+   */
+  void
+  setPositiveLimitConnector(eipScanner::cip::CipSint positiveLimitConnector);
+
+  /**
+   * @brief Sets NegativeLimitConnector [AttrID=5]
+   * @return
+   */
+  void
+  setNegativeLimitConnector(eipScanner::cip::CipSint negativeLimitConnector);
+
+  /**
+   * @brief Sets HomeSensorConnector [AttrID=6]
+   * @return
+   */
+  void setHomeSensorConnector(eipScanner::cip::CipSint homeSensorConnector);
+
+  /**
+   * @brief Sets BrakeOutputConnector [AttrID=7]
+   * @return
+   */
+  void setBrakeOutputConnector(eipScanner::cip::CipSint brakeOutputConnector);
+
+  /**
+   * @brief Sets TriggerPositionCaptureConnector [AttrID=8]
+   * @return
+   */
+  void setTriggerPositionCaptureConnector(
+      eipScanner::cip::CipSint triggerPositionCaptureConnector);
+
+  /**
+   * @brief Sets MaxDecelerationRate [AttrID=9]
+   * @return
+   */
+  void setMaxDeceleration(eipScanner::cip::CipDint maxDecelerationRate);
+
+  /**
+   * @brief Sets StopSensorConnector [AttrID=10]
+   * @return
+   */
+  void setStopSensorConnector(eipScanner::cip::CipSint stopSensorConnector);
+
+  /**
+   * @brief Sets FollowEncoderMode [AttrID=11]
+   * @return
+   */
+  void setFollowAxis(eipScanner::cip::CipSint followEncoderMode);
+
+  /**
+   * @brief Sets FollowDivisor [AttrID=12]
+   * @return
+   */
+  void setFollowDivisor(eipScanner::cip::CipDint followDivisor);
+
+  /**
+   * @brief Sets FollowMultiplier [AttrID=13]
+   * @return
+   */
+  void setFollowMultiplier(eipScanner::cip::CipDint followMultiplier);
+
 private:
   std::vector<DataFieldReference> _getDataFieldReferences() override;
 
